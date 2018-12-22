48425c71c7a4790a7900849026c07673eb424932
giahuymcn <46061467+giahuymcn@users.noreply.github.com>
2018-12-22 10:32:06 +0700

    Add files via upload

---
+++ KinematicOutput.java
@@ -0,0 +1,41 @@
+package movement;
+
+public class KinematicOutput {
+    
+    private Vector2D velocity;
+    private float rotation;
+
+    public KinematicOutput() {
+    }
+
+    public KinematicOutput(Vector2D velocity, float rotation) {
+        this.velocity = velocity;
+        this.rotation = rotation;
+    }
+
+    public Vector2D getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2D velocity) {
+        this.velocity = velocity;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(float rotation) {
+        this.rotation = rotation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return "KinematicOutput{" + "velocity=" + velocity + ", rotation=" + rotation + '}';
+    }
+}
